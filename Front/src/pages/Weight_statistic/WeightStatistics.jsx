28eb5cfc549f26025cfcbd13a79341d985ee6a38
Вадим <114065342+AutomageVadimHerasimovich@users.noreply.github.com>
2025-03-18 12:55:50 +0300

    Update WeightStatistics.jsx

--- Front/src/pages/Weight_statistic/WeightStatistics.jsx
+++ Front/src/pages/Weight_statistic/WeightStatistics.jsx
@@ -670,7 +670,7 @@ useEffect(() => {
                     </div>
                   </div>
                 </div>
-              </div>>
+              </div>
             </div>
           </div>
         </div>
