1042083d2c2eaa20bac82882614424a254339e0b
amor <159952777+amor0009@users.noreply.github.com>
2025-03-22 19:18:59 +0300

    Delete Front directory

--- Front/src/utils/auth.js
+++
@@ -1,8 +0,0 @@
-// utils/auth.js
-export const checkAuth = () => {
-    const token = localStorage.getItem("access_token");
-    if (!token) {
-      return false; // Пользователь не авторизован
-    }
-    return true; // Пользователь авторизован
-  };
